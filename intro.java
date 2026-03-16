d99301f04c4a62425fb7bea039e795b01174f00f
Sumit Kumar <sumitkumar18950@gmail.com>
2026-03-16 14:31:44 +0530

    Create StudentDAO interface for student operations

---
+++ intro.java
@@ -0,0 +1,19 @@
+package com.crudjpa.crudjpa.dao;
+
+import com.crudjpa.crudjpa.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+   List<Student> findAll();
+
+    Student findById(int theId);
+
+    Student save(Student theStudent);
+
+    void deleteById(int theId);
+
+
+
+}
